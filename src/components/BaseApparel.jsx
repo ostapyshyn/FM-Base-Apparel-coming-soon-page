b72bf71bd331a5ca1653bb71508832d44377d067
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-10-02 23:55:21 +0300

    Add basic styles

---
+++ src/components/BaseApparel.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function BaseApparel() {
+  return (
+    <div>
+      <p>WE'RE COMINGSOON</p>
+      <p>
+        Hello fellow shoppers! We're currently building our new fashion store. Add your email below
+        to stay up-to-date with announcements and our launch deals.
+      </p>
+    </div>
+  );
+}
+
+export default BaseApparel;
