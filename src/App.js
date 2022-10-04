23bc81608e2fea728e1a991ab2e2ff6205b193a2
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-10-04 23:48:28 +0300

    Start mobile styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import BaseApparel from './components/BaseApparel';
 
 function App() {
--- src/components/BaseApparel.jsx
+++ src/components/BaseApparel.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../assets/baseApparel.module.scss';
 
 import { ReactComponent as Logo } from '../assets/svg/logo.svg';
 import { ReactComponent as Arrow } from '../assets/svg/icon-arrow.svg';
+import { ReactComponent as Error } from '../assets/svg/icon-error.svg';
 import hero_desktop from '../assets/hero-desktop.jpg';
 import hero_mobile from '../assets/hero-mobile.jpg';
 
 function BaseApparel() {
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleOnChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const emailValidation = () => {
+    const regEx = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+    if (!regEx.test(email) && email !== '') {
+      setMessage('Please provide a valid email');
+    } else if (email === '') {
+      setMessage('Please provide a valid email');
+    } else {
+      setMessage('');
+      setEmail('');
+    }
+  };
+
   return (
     <section>
       <article>
@@ -18,10 +38,21 @@ function BaseApparel() {
           Hello fellow shoppers! We're currently building our new fashion store. Add your email
           below to stay up-to-date with announcements and our launch deals.
         </p>
-        <input type="text" placeholder="Email Address" />
-        <button>
-          <Arrow />
-        </button>
+        <div className={styles.input_field}>
+          <input
+            id="email"
+            type="email"
+            value={email}
+            onChange={handleOnChange}
+            placeholder="Email Address"
+          />
+          {message && <Error />}
+          <p className={styles.error}>{message}</p>
+
+          <button onClick={emailValidation}>
+            <Arrow />
+          </button>
+        </div>
       </article>
 
       <picture>
